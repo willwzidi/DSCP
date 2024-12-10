810e88eb88398dedf57eecb0d3a64bdba8dc789d
hwu478 <hwu478@learn.chtc.wisc.edu>
2024-12-10 11:35:01 -0600

    read csv name to csv_list

--- read_csv_name.sh
+++ read_csv_name.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls /home/groups/STAT_DSCP/Group6/flight | head -n 100 | sed -s "s|.csv||" > csv_list
+ls /home/groups/STAT_DSCP/Group6/Flight | head -n 450 | sed -s "s|.csv||" > csv_list
